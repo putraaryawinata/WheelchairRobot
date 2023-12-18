910225522a5e770f25b8882598add2a0e99baaf7
putraaryawinata <aryaw2001@gmail.com>
2023-12-18 15:03:16 +0800

    ekf

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input, Conv1D
+from keras.models import Sequential, Model
+from keras.layers import LSTM, RepeatVector, TimeDistributed, Dense, Input, Flatten
 
 if __name__ == "__main__":
     print("Training starts!")
