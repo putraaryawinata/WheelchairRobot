55b9351f3f63a75ed580d50100eb921915ceeb1a
putraaryawinata <aryaw2001@gmail.com>
2024-02-27 17:19:56 +0700

    update pos name data

--- data/npy2csv.py
+++ data/npy2csv.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 name = ["ekf_auto_pos", "ekf_pos", "noise_pos", "auto_pos"]
-for n in name:
-    arr = np.load(f"{n}.npy")
-    np.savetxt(f"{n}.csv", arr[0], delimiter=",")
+new_name = ["noise_pos", "ekf_pos", "auto_pos", "ekf_auto_pos"]
+for i in range(len(name)):
+    arr = np.load(f"{name[i]}.npy")
+    np.savetxt(f"{new_name[i]}.csv", arr[0], delimiter=",")
