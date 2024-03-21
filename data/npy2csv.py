d76f5251b1482105b2f798455880a251f99606d0
putraaryawinata <aryaw2001@gmail.com>
2024-03-21 20:08:01 +0900

    add all data

--- data/npy2csv.py
+++ data/npy2csv.py
@@ -2,6 +2,11 @@ import numpy as np
 
 name = ["ekf_auto_pos", "ekf_pos", "noise_pos", "auto_pos"]
 new_name = ["noise_pos", "ekf_pos", "auto_pos", "ekf_auto_pos"]
-for i in range(len(name)):
-    arr = np.load(f"{name[i]}.npy")
-    np.savetxt(f"{new_name[i]}.csv", arr[0], delimiter=",")
+for index in range(200):
+    for i in range(len(name)):
+        arr = np.load(f"{name[i]}.npy")
+        np.savetxt(f"all/{new_name[i]}_{index}.csv", arr[index], delimiter=",")
+
+# arr = np.load(f"svm_y.npy")
+# print(arr.shape)
+# np.savetxt(f"svm_y.csv", arr, delimiter=",")
